cae77a91075e74017fe925240c14b1fd946eb3cb
Darlington6 <d.tunyinko@alustudent.com>
2024-06-29 11:22:47 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com along with an employee ID to
-return information about the employee's todo list progress.
-"""
+"""Module"""
 
 import requests
-from sys import argv
+import sys
+
+"""Module"""
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        print("Usage: ./script.py <employee_id>")
-        exit(1)
+    """IF SCRIPT IS NOT RUN AS MODULE"""
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
-    user_id = argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    user_info = requests.get(user_url).json()
+    todos_info = requests.get(todos_url).json()
 
-    user = requests.get(user_url, verify=False).json()
-    todos = requests.get(todo_url, verify=False).json()
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-    completed_tasks = [task['title'] for task in todos if task.get('completed')]
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    employee_name = user.get('name')
-    total_tasks = len(todos)
-    completed_count = len(completed_tasks)
-
-    print(f"Employee {employee_name} is done with tasks({completed_count}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task}")
+    [print("\t " + task["title"]) for task in task_completed]
